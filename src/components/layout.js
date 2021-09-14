b96b953243d9483ce7dd300629628f50d92ac952
George_Chakama <gtchakama@gmail.com>
2021-09-14 21:08:45 +0200

    Update layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,17 +1,29 @@
 import * as React from "react";
 import { Link } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 import {
   container,
   heading,
   navLinks,
   navLinkItem,
   navLinkText,
+  siteTitle,
 } from "./layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
   return (
     <div className={container}>
-      <title>{pageTitle}</title>
+      <header className={siteTitle}>{data.site.siteMetadata.title}</header>
+      {/* <title>{pageTitle}</title> */}
       <nav>
         <ul className={navLinks}>
           <li className={navLinkItem}>
