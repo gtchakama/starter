42dc4cd7b070a1fba29c36664630e50ba860c69a
George_Chakama <gtchakama@gmail.com>
2021-09-14 16:47:28 +0200

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,10 @@
 module.exports = {
     siteMetadata: {
+        title: "Project Starter",
         siteUrl: `https://www.yourdomain.tld`,
     },
     plugins: [
-
+        "gatsby-plugin-image",
+    "gatsby-plugin-sharp",
     ]
 }
