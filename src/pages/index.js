ba57539fac1047ab11255efe61af5418c74102ba
George_Chakama <gtchakama@gmail.com>
2021-09-14 14:13:04 +0200

    link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,14 @@
 // Step 1: Import React
 import * as React from "react";
-
+import { Link } from "gatsby";
 // Step 2: Define your component
 const IndexPage = () => {
   return (
     <main>
+      <Link to="/about">About</Link>
       <title>Home Page</title>
-      <h1>Welcome to my Gatsby site!</h1>
-      <p>I'm making this by following the Gatsby Tutorial.</p>
+      <h1>Welcome to my Project Sarter!</h1>
+      <p>I'm making this to help me structure my C-R-A alternative.</p>
     </main>
   );
 };
