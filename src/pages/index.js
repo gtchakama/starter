6903925c3ec716bf054e3948c6a4272242cb1d61
George_Chakama <gtchakama@gmail.com>
2021-09-14 14:24:33 +0200

    layout

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,12 @@
 // Step 1: Import React
 import * as React from "react";
-import { Link } from "gatsby";
+import Layout from "../components/layout";
 // Step 2: Define your component
 const IndexPage = () => {
   return (
-    <main>
-      <Link to="/about">About</Link>
-      <title>Home Page</title>
-      <h1>Welcome to my Project Sarter!</h1>
+    <Layout pageTitle="Home Page">
       <p>I'm making this to help me structure my C-R-A alternative.</p>
-    </main>
+    </Layout>
   );
 };
 
